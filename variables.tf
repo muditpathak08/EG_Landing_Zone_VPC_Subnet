02e33e035b085e25b56c01bdc9dddc0dc453d740
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-12-13 19:09:43 +0530

    Update variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "service" {
+  type        = string
+  description = "Application Name"
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+  type = string
+}
+
+variable "public_subnets" {
+  type = list
+  description = "List of public subnets"
+}
+
+variable "private_subnets" {
+  type =list
+  description = "List of private subnets"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+}
