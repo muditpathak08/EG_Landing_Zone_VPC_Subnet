9b5650c6f14c02f3ac00d16397bcabfc6e188ae4
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-12-19 17:32:05 +0530

    Update network.tf

--- modules/network/network.tf
+++ modules/network/network.tf
@@ -68,8 +68,8 @@ resource "aws_eip" "eip" {
 ## Nat Gateway
 ##############
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.eip.id
-  subnet_id      =  flatten(aws_subnet.public.id)[0]
+  allocation_id  = aws_eip.eip.id
+  subnet_id      = aws_subnet.public[0].id
   depends_on = [aws_eip.eip, aws_internet_gateway.aws-igw, aws_subnet.public]
   tags ={
     Name = "${var.service}-ngw"
